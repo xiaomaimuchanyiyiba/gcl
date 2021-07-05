9f3a924646ef6bcdce18e2ad0013ad6cfe1e31ed
ポ④ふмαIмù産ーィＺ⒏ず <xiaomaimuchanyiyiba@outlook.com>
2021-07-05 16:16:04 +0800

    Update gcl.sh

--- gcl.sh
+++ gcl.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-set -euo pipefail
+set -uo pipefail
 
 trap "echo 'something wrong'" ERR
 function initAsHTTPUrl() {
@@ -35,8 +35,17 @@ function clone() {
     mkdir -p ${DEST_DIR}
     git clone ${URL} ${DEST_DIR}
 }
+
+function postProcess() {
+    read -p "Do you wish to open ${DEST_DIR} with vscode ?" OPEN_WITH_EDITOR
+    if [[ ${OPEN_WITH_EDITOR} == "y" ]]; then
+        code ${DEST_DIR}
+    fi
+}
+
 WORKSPACE=workspace
 URL=$1
 
 init
 clone
+postProcess
